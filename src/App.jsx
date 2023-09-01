b943abfa0984c2053fd6d009689507c401ce18ba
Juan Ceballos <juan.ceballos@artifex.co>
2023-09-01 15:01:20 -0500

    Import hero component and add it to te app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 import './App.css';
 
 const App = () => {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
     </div>
   );
 };

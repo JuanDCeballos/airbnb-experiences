c68f0536e1eb84966026b6c66b811e34aa95e474
Juan Ceballos <juan.ceballos@artifex.co>
2023-09-01 11:51:59 -0500

    Add new navbar component with the structure and classNames to be able to modify em in css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import AirbnbLogo from '../assets/airbnb.png';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar-container">
+      <img src={AirbnbLogo} />
+    </nav>
+  );
+};
+
+export default Navbar;

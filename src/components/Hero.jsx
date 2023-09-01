ca04ccd5773b0ca1e682d84345571d71cba636da
Juan Ceballos <juan.ceballos@artifex.co>
2023-09-01 14:59:43 -0500

    Add new hero component with their respective structure

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import experiences from '../assets/Experiences.png';
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-container-image">
+        <img src={experiences} className="img" />
+      </div>
+      <div className="hero-container-info">
+        <h1 className="hero-container-title">Online Experiences</h1>
+        <p className="hero-container-desc">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;

cc3770c9d53b62dcd0ecffae7a3ebad3f5032a33
Juan Ceballos <juan.ceballos@artifex.co>
2023-09-01 11:54:53 -0500

    Cleaned the structure and import navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from './components/Navbar';
 import './App.css';
 
 const App = () => {
   return (
-    <>
-      <h1>a</h1>
-    </>
+    <div className="container">
+      <Navbar />
+    </div>
   );
 };
 

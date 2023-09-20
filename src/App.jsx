8e2d8ad4217ea845ad05b1a5200e7623494c98ec
Juan Ceballos <juandavi789@hotmail.com>
2023-09-19 23:46:12 -0500

    Add new feature to render the cards iterating over a map

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import data from './data';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="container">
+    <div className='container'>
       <Navbar />
       <Hero />
-      <Card />
+      <section className='cards-list'>
+        {data.map((item) => (
+          <Card
+            key={item.id}
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            price={item.price}
+          />
+        ))}
+      </section>
     </div>
   );
 };

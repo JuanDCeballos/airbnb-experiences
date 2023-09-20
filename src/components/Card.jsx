5bc4922fa746b73b91ed13347e1a75f491e80383
Juan Ceballos <juandavi789@hotmail.com>
2023-09-19 23:47:04 -0500

    Update card component with some css classes, the way we put the text, we recive em from the app.js via props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,25 @@
-import Swimming from '../assets/Swimming.png';
 import Star from '../assets/Star.png';
 
-const Card = () => {
+const Card = ({ img, rating, reviewCount, location, title, price }) => {
   return (
-    <div className="card-container">
-      <div className="card-container-main">
-        <img src={Swimming} className="card-container-img" />
-        <p className="card-container-status">SOLD OUT</p>
+    <div className='card-container'>
+      <div className='card-container-main'>
+        <img src={`/${img}`} className='card-container-img' />
+        <p className='card-container-status'>SOLD OUT</p>
       </div>
       <div>
-        <div className="card-container-rating">
-          <img src={Star} className="card-container-rating-star" />
-          <p className="text"> 5.0 </p>
-          <p className="text"> (6) </p>
-          <p className="text"> · </p>
-          <p className="text"> USA </p>
+        <div className='card-container-rating'>
+          <img src={Star} className='card-container-rating-star' />
+          <p className='text'> {rating} </p>
+          <p className='text'> {`(${reviewCount})`} </p>
+          <p className='text'> • </p>
+          <p className='text'> {location} </p>
         </div>
-        <p className="text">Life lessons with Katie Zaferes</p>
-        <div className="card-container-desc">
-          <p className="text">From $</p>
-          <p className="text">136 </p>
-          <p className="text"> / person</p>
+        <p className='text'>{title}</p>
+        <div className='card-container-desc'>
+          <p className='text'>From $</p>
+          <p className='text'>{price} </p>
+          <p className='text'> / person</p>
         </div>
       </div>
     </div>

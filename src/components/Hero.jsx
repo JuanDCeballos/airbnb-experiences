9e5ffcbf9869fce92f4f0cefccef5b64f6090ae9
Juan Ceballos <juandavi789@hotmail.com>
2023-09-19 23:45:15 -0500

    Update hero component with some css classes to be able to style it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ import experiences from '../assets/Experiences.png';
 
 const Hero = () => {
   return (
-    <div className="hero-container">
-      <div className="hero-container-image">
-        <img src={experiences} className="img" />
+    <div className='hero-container'>
+      <div className='hero-container-image'>
+        <img src={experiences} className='img' />
       </div>
-      <div className="hero-container-info">
-        <h1 className="hero-container-title">Online Experiences</h1>
-        <p className="hero-container-desc">
+      <div className='hero-container-info'>
+        <h1 className='hero-container-title'>Online Experiences</h1>
+        <p className='hero-container-desc'>
           Join unique interactive activities led by one-of-a-kind hosts—all
           without leaving home.
         </p>

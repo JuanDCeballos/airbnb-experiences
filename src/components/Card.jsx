36fbf3199c039462b5ee37e0c0cd4c7bd9cbc1ca
Juan Ceballos <juandavi789@hotmail.com>
2023-09-24 10:53:38 -0500

    Add conditional rendering to the card component when the open spost are equal to zero or the event is online, update the way we ask for the props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,33 @@
 import Star from '../assets/Star.png';
 
-const Card = ({ img, rating, reviewCount, location, title, price }) => {
+const Card = (items) => {
+  let badgeText;
+  if (items.items.openSpots === 0) {
+    badgeText = 'SOLD OUT';
+  } else if (items.items.location === 'Online') {
+    badgeText = 'ONLINE';
+  }
+
   return (
     <div className='card-container'>
+      {badgeText !== undefined && (
+        <div className='card-container-status'>{badgeText}</div>
+      )}
       <div className='card-container-main'>
-        <img src={`/${img}`} className='card-container-img' />
-        <p className='card-container-status'>SOLD OUT</p>
+        <img src={`/${items.items.coverImg}`} className='card-container-img' />
       </div>
       <div>
         <div className='card-container-rating'>
           <img src={Star} className='card-container-rating-star' />
-          <p className='text'> {rating} </p>
-          <p className='text'> {`(${reviewCount})`} </p>
+          <p className='text'> {items.items.stats.rating} </p>
+          <p className='text'> {`(${items.items.stats.reviewCount})`} </p>
           <p className='text'> • </p>
-          <p className='text'> {location} </p>
+          <p className='text'> {items.items.location} </p>
         </div>
-        <p className='text'>{title}</p>
+        <p className='text'>{items.items.title}</p>
         <div className='card-container-desc'>
           <p className='text'>From $</p>
-          <p className='text'>{price} </p>
+          <p className='text'>{items.items.price} </p>
           <p className='text'> / person</p>
         </div>
       </div>
